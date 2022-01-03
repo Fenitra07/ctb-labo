d6edd242b09302d22b178dd2bc3afc18abd2b7c2
HaryFenitra <fenitrar07@gmail.com>
2022-01-03 11:35:54 +0300

    v03012022 à 11:35

---
+++ recuperationresultat.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>ctb-labo.mg</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon-sante.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  <div id="topbar" class="d-flex align-items-center fixed-top">
+    <div class="container d-flex justify-content-between">
+      <div class="contact-info d-flex align-items-center">
+        <i class="bi bi-envelope"></i> <a href="mailto:contact@example.com">ctb.communication@ctb.mg</a>
+        <i class="bi bi-phone"></i> +261 34 44 444 44
+      </div>
+<!--       <div class="d-none d-lg-flex social-links align-items-center">
+        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+      </div> -->
+    </div>
+  </div>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto">
+        <!-- <a href="index.html">ctb-labo.mg</a> -->
+        <a href="index.html" class="logo me-auto"><img src="assets/img/logo-ctb.jpg" alt="" class="img-fluid"></a>
+      </h1>
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto" href="index.php">Accueil</a></li>
+          <li><a class="nav-link scrollto" href="services.php">A propos</a></li>
+          <li class="dropdown"><a href="noslaboratoires.php"><span>Nos laboratoires</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="noslaboratoires.php">CTB Andraharo</a></li>
+              <li><a href="noslaboratoires.php">CTB Ivato</a></li>
+              <li><a href="noslaboratoires.php">CTB Toamasina</a></li>
+              <li><a href="noslaboratoires.php">CTB Mahajanga</a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="" class="active"><span>Je veux faire des analyses</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="cdtprelevement.php">Conditions de prélèvement</a></li>
+              <li><a href="pecassurance.php">Prise en charge assurance</a></li>
+              <li><a href="recuperationresultat.php">Récupération des résultats</a></li>
+              <li><a href="ctlanalyse.php">Nos catalogues d'analyses</a></li>
+              <li><a href="analysecovid.php">Informations et Analyses Covid 19</a></li>
+              <li><a href="devis.php">Demande de devis</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="espacepro.php">Espace professionnel</a></li>
+          <li><a class="nav-link scrollto" href="blogsante.php">Blog santé</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <a href="devis.php" class="appointment-btn scrollto"><span class="d-none d-md-inline">Demande de </span> devis</a>
+
+    </div>
+  </header><!-- End Header -->
+
+  <main id="main">
+
+     <!-- ======= Breadcrumbs Section ======= -->
+    <section class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <!-- <h2>Inner Page</h2> -->
+          <ol>
+            <li><a href="services.php">Je veux faire des analyses</a></li>
+            <li>Récupération des résultats</li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End Breadcrumbs Section -->
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Récupération des résultats</h2>
+          <p>Le centre technique biomédical vous propose 3 moyens de récupérer vos résultats :</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="icon-box">
+              <div class="icon"><i class="fas fa-hand-holding-medical"></i></div>
+              <h4><a href="">En main propre</a></h4>
+              <p style="text-align: justify;">Vous pouvez récupérer vos résultats d’analyses directement auprès de l’accueil si vous avez besoin d’une version sur papier.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+            <div class="icon-box">
+              <div class="icon"><i class="fas fa-mail-bulk"></i></div>
+              <h4><a href="">Par mail</a></h4>
+              <p style="text-align: justify;">Nous sommes également disposés à vous les envoyer par messagerie électronique si vous avez une ou plusieurs adresses. Par ailleurs, votre prescripteur peut également recevoir une copie si son adresse est inscrite sur notre base de données.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+            <div class="icon-box">
+              <div class="icon"><i class="fas fa-hospital-user"></i></div>
+              <h4><a href="">Sur le serveur des résultats</a></h4>
+              <p style="text-align: justify;">L’avantage est d’avoir ses résultats médicaux à disposition en temps réel, dès qu’ils ont été validés. Pour cela, il vous suffit de vous connecter sur notre serveur de résultats. Les résultats sur le serveur sont totalement confidentiels, seul vous qui êtes en possession de l’identifiant et du mot de passe pouvez y avoir accès.</p>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="row">
+          <div class="col-lg-12 col-md-12 mt-4">
+	         <p style="text-align: justify;">Une fois vos résultats disponibles, vous pouvez passer à toute heure pour les récupérer, pendant les heures d’ouverture du laboratoire. Il est possible de récupérer vos résultats dans tous les laboratoires et centre de prélèvement du Centre Technique Biomédical à Madagascar.
+	         </p>
+	         <p style="text-align: justify;">
+	         	Si vous avez perdu votre exemplaire, vous pouvez faire la demande d’une copie à l’accueil, mais il vous faut emmener une pièce justificative (Facture).
+	         </p>
+	         <p style="text-align: justify;">
+	         	Exceptionnellement, vos résultats médicaux peuvent être communiqués par téléphone uniquement si c’est nécessaire pour un suivi thérapeutique. Cela concerne uniquement : la glycémie, la créatinine, le TP/INR et les D-Dimères.
+	         </p>
+	         <p style="text-align: justify;">
+	         	D’autre part, nous pouvons envoyer vos résultats d’analyses par mail directement chez votre médecin prescripteur afin de vous éviter un déplacement supplémentaire.
+	         </p>
+	 	   </div>
+        </div>
+
+
+      </div>
+    </section><!-- End Services Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6 footer-contact">
+            <h3>ctb-labo</h3>
+            <p>
+              Adresse principale <br>
+              Ville<br>
+              Pays <br><br>
+              <strong>Téléphone:</strong> +261 34 44 444 44<br>
+              <strong>Email:</strong> ctb.communication@ctb.mg<br>
+            </p>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-links">
+            <h4>Nos Laboratoires</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="noslaboratoires.php">CTB Andraharo</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="noslaboratoires.php">CTB Ivato</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="noslaboratoires.php">CTB Toamasina</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="noslaboratoires.php">CTB Mahajanga</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-links">
+            <h4>Je veux faire des analyses</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="cdtprelevement.php">Conditions de prélèvement</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="pecassurance.php">Prise en charge assurance</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="recuperationresultat.php">Récupération des résultats</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="ctlanalyse.php">Nos catalogues d'analyses</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="analysecovid.php">Informations et Analyses Covid 19</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="devis.php">Demande de devis</a></li>
+            </ul>
+          </div>
+<!-- 
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+            <h4>Newsletter</h4>
+            <p>Suivez-nous</p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form>
+          </div> -->
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container d-md-flex py-4">
+
+      <div class="me-md-auto text-center text-md-start">
+        <div class="copyright">
+          &copy; Copyright <strong><span>ctb-labo.mg</span></strong>. Tous droits réservés
+        </div>
+<!--         <div class="credits">
+          Made with passion by <a target="_blank" href="https://pricing.dev-inbox.com/">Devinbox</a>
+        </div> -->
+      </div>
+      <div class="social-links text-center text-md-right pt-3 pt-md-0">
+        <!-- <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a> -->
+        <a href="" class="facebook" target="_blank"><i class="bx bxl-facebook"></i></a>
+        <!-- <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a> -->
+        <a href="" class="google-plus" target="_blank"><i class="bx bxl-skype"></i></a>
+        <a href="" class="linkedin" target="_blank"><i class="bx bxl-linkedin"></i></a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
