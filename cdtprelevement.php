907b41f3394ccfe5abb5ef170b1265fb1a647671
HaryFenitra <fenitrar07@gmail.com>
2022-03-17 21:44:22 +0300

    v17032022 à 21:44 par Fenitra

--- cdtprelevement.php
+++ cdtprelevement.php
@@ -70,7 +70,7 @@
               <li><a href="noslaboratoires.php">CTB Mahajanga</a></li>
             </ul>
           </li>
-          <li class="dropdown"><a href=""><span>Je veux faire des analyses</span> <i class="bi bi-chevron-down"></i></a>
+          <li class="dropdown"><a href="" class="active"><span>Je veux faire des analyses</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
               <li><a href="cdtprelevement.php">Conditions de prélèvement</a></li>
               <li><a href="pecassurance.php">Prise en charge assurance</a></li>
@@ -81,7 +81,7 @@
             </ul>
           </li>
           <li><a class="nav-link scrollto" href="espacepro.php">Espace professionnel</a></li>
-          <li><a class="nav-link scrollto active" href="blogsante.php">Blog santé</a></li>
+          <li><a class="nav-link scrollto" href="blogsante.php">Blog santé</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
@@ -113,11 +113,215 @@
       <div class="container">
 
         <div class="section-title">
-          <h2>
-            Cette page est en cours de conception... <br>
-            <i class="fa fa-spinner"></i>
-          </h2>
-          <!-- <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p> -->
+          <h2>Conditions de prélèvement</h2>
+          <!-- <p style="text-align: center;">En cette période de pandémie, le laboratoire CTB propose 2 différents tests pour le Covid-19.</p> -->
+        </div>
+      
+        <div class="faq-list">
+
+          <h5 class="analyse_titre_h5">I. Les analyses strictement à jeun  :</h5>
+          <p style="text-align: justify;">
+          Pour certaines analyses, il est indispensable d’être à jeun, c’est-à-dire que vous ne devez prendre ni de la nourriture solide, ni liquide depuis au moins 10 heures de temps. Sont concernées dans ces tests : 
+          </p>
+          <ul>
+            <li>La glycémie</li>
+            <li>La cholestérolémie (fractions HDL et LDL)</li>
+            <li>La triglycéridémie</li>
+            <li>L’uricémie</li>
+            <li>La calcémie</li>
+            <li>La phosphorémie</li>
+            <li>Iget</li>
+            <li>CLA30</li>
+          </ul>
+
+          <h5 class="analyse_titre_h5">II.  Les épreuves de l’hypoglycémie provoquée :</h5>
+          <p style="text-align: justify;">
+          Pour le dépistage du diabète gestationnel (HGPO), les conditions de jeun total doivent être respectées. Mais en plus, le patient doit rester au laboratoire pendant au moins 2h de temps. Cela, parce que le prélèvement se passe en plusieurs étapes : 
+          </p>
+          <ul>
+            <li>Un premier prélèvement pour la glycémie à jeun</li>
+            <li>Ingestion de 75g de sucre dilué dans 500 ml d’eau</li>
+            <li>Un deuxième prélèvement 1 h après ingestion de la solution sucrée</li>
+            <li>Un troisième prélèvement 2 h après ingestion de la solution sucrée</li>
+          </ul>
+          <p style="text-align: justify;">
+          D’autre part, le médecin peut demander le test O’ Sullivan comme test de diagnostic de diabète gestationnel. Le patient doit également être totalement à jeun depuis la veille et prévoir 1h de temps pour effectuer le test. Il se déroule également en plusieurs étapes : 
+          </p>
+          <ul>
+            <li>Un premier prélèvement pour la glycémie à jeun</li>
+            <li>Ingestion de 50g de sucre dilué dans 500 ml d’eau</li>
+            <li>Un deuxième prélèvement 1 h après ingestion de la solution sucrée</li>
+          </ul>
+
+          <h5 class="analyse_titre_h5">III. Les analyses où il est préférable d’être à jeun :</h5>
+          <p style="text-align: justify;">
+          Pour d’autres analyses, il est préférable d’être à jeun, bien que ce ne soit pas obligatoire. Cela concerne : 
+          </p>
+          <ul>
+            <li>Le Fer</li>
+            <li>La créatininémie</li>
+            <li>La CRP</li>
+            <li>L’ionogramme</li>
+            <li>Les enzymes hépatiques : ALAT/ASAT (transaminases), GGT, Bilirubines (BTC), Phosphatases Alcalines (PAL)</li>
+            <li>Les enzymes cardiaques : Lactico-déshydrogénase (LDH), CK-MB (CKMB)</li>
+            <li>La numération (NFS)</li>
+            <li>La vitesse de SEDIMENTATION (VS)</li>
+            <li>Les tests de coagulation : Taux de Prothrombine (TP) + INR, Fibrinogène ou Facteur I (FIB), Temps de Céphaline Activée ou Temps de Quick (TCA), Temps de Saignement (TS)</li>
+            <li>L’électrophorèse des protéines sériques (ELPR)</li>
+          </ul>
+
+          <h5 class="analyse_titre_h5">IV.  Les analyses qui ne nécessitent pas d’être à jeun  :</h5>
+          <p style="text-align: justify;">
+          Le jeun est inutile pour les analyses sérologiques ainsi que les marqueurs tumoraux, car l’alimentation n’influence pas les résultats. Vous n’avez pas besoin d’être à jeun pour l’analyse de : 
+          </p>
+          <ul>
+            <li>La syphilis</li>
+            <li>Le VIH</li>
+            <li>Les hépatites A, B et C</li>
+            <li>La toxoplasmose</li>
+            <li>La rubéole</li>
+            <li>La fièvre typhoïde</li>
+            <li>La salmonellose</li>
+            <li>L’amibiase</li>
+            <li>La bilharziose</li>
+            <li>La cysticercose</li>
+            <li>Les marqueurs tumoraux : PSA, ACE, AFP, CA125, CA153, CA199</li>
+          </ul>
+
+          <h5 class="analyse_titre_h5">V. Recueil des urines de 24 heures :</h5>
+          <ul>
+            <li>Au lever, éliminer les premières urines dans les toilettes.</li>
+            <li>Commencer le recueil des urines sur la prochaine miction en notant bien l’heure du début.</li>
+            <li>Continuer à recueillir toutes les urines pendant les 24 heures qui suivent. C’est-à dire jusqu’à la même heure le lendemain. Utilisez des bouteilles bien propres et n’ayant pas contenu de liquide sucré pour le recueil. Si possible, gardez les urines recueillies dans un endroit frais. Quand vous avez complétez les 24 heures de recueil, apportez rapidement toutes les urines au laboratoire.</li>
+          </ul>
+          <p style="text-align: justify;">
+            Le dosage de plusieurs paramètres biochimiques tels que : l’acide urique urinaire, le calcium urinaire, le cortisol urinaire, la créatinine urinaire, le glycosurie, l’ionogramme urinaire, le magnésium urinaire, microalbuminurie, le phosphore urinaire, protéinurie et urée urinaire ne se font que sur les urines de 24 heures.  
+          </p>
+
+          <h5 class="analyse_titre_h5">VI.  Recueil d’urines pour le « COMPTE D’ADDIS » (H.L.M.) :</h5>
+          <p style="text-align: justify;">
+            Pour le compte d’ADDIS ou culot HLM, il faut procéder comme suit :  
+          </p>
+          <ul>
+            <li>Se réveiller 3 heures plus tôt que d’habitude</li>
+            <li>Se débarrasser des premières urines du matin dans les toilettes</li>
+            <li>Boire un grand verre d’eau, au plus 250ml</li>
+            <li>Mettre un réveil pour 3 heures, puis se rendormir</li>
+            <li>Au réveil, recueillir toutes les urines dans un flacon ou une bouteille bien propre et n’ayant pas contenu de liquide sucré.</li>
+          </ul>
+
+          <h5 class="analyse_titre_h5">VII. Recueil de sperme en vue d’un spermogramme ou d’une spermoculture :</h5>
+          <ul>
+            <li><b>Les conditions à respecter pour le recueil :</b></li>
+          </ul>
+          <p style="text-align: justify;">
+            Le recueil de sperme s’effectue uniquement au laboratoire afin d’éviter tout risque d’altération des spermatozoïdes pendant le transport. <br>
+            Si toutefois, vous devez l’effectuer à domicile, il faut acheminer le recueil dans un délai de moins de 30 minutes, à l’abri de la lumière et à une température de 37 °C. <br>
+            Il faut respecter une abstinence sexuelle d’au moins 3 jours pour pouvoir faire le recueil. 
+            S’il s’agit d’un recueil de sperme pour un spermoculture, il faut respecter un délai de 10 jours après un traitement antibiotique avant de le réaliser. Par ailleurs, le recueil peut être réaliser à domicile pour cet examen. 
+  
+          </p>
+          <ul>
+            <li><b>Le recueil proprement dit :</b></li>
+          </ul>
+          <p style="text-align: justify;">
+            Uriner avant de réaliser le recueil.<br>
+            Bien se laver les mains et la verge avec de l’eau<br>
+            Pratiquer la masturbation pour faire le recueil dans un flacon stérile <br>
+            Le laboratoire CTB peut recevoir une demande de spermogramme du lundi au vendredi, de 7h à 10h. 
+          </p>
+
+          <h5 class="analyse_titre_h5">VIII.  Examen cytobactériologique des Urines (ECBU) :</h5>
+          <p style="text-align: justify;">
+            Le recueil doit se faire sur les premières urines du matin et dans un flacon stérile fournit par le laboratoire CTB.   
+          </p>
+          <p style="text-align: justify;">
+            <b>Comment procéder :</b>   
+          </p>
+          <ul>
+            <li>Bien se laver les mains</li>
+            <li>Procéder à la toilette intime habituelle</li>
+            <li>Eliminer le premier jet</li>
+            <li>Recueillir dans le flacon stérile le jet du milieu en prenant soin de ne pas toucher les rebords du flacon</li>
+            <li>Fermer le flacon hermétiquement et présenter l’échantillon à l’accueil du laboratoire dans les plus brefs délais</li>
+          </ul>
+          <p style="text-align: justify;">
+            <b>Si le patient est sondé à domicile ou en milieu hospitalier :</b>    
+          </p>
+          <ul>
+            <li>Si possible, faire le recueil uniquement lors du changement de la sonde…</li>
+            <li>Si ce n’est pas possible, commencer par clamper la tubulure en aval afin de permettre un recueil dans une nouvelle miction. Dès qu’une quantité d’urines jugée suffisante s’accumule dans la tubulure, procéder comme pour un prélèvement à la seringue. Désinfecter la tubulure, puis prélever les urines avec une aiguille et une seringue stérile. Transférer les urines recueillies dans le flacon stérile fourni par le laboratoire. Apporter l’échantillon au laboratoire dans les plus brefs délais.</li>
+          </ul>
+
+          <h5 class="analyse_titre_h5">IX. Recommandations pour les prélèvements gynécologiques :</h5>
+          <ul>
+            <li><b>Pour un prélèvement vaginal ou cervico-vaginal :</b></li>
+          </ul>
+          <p style="text-align: justify;">
+            Pas de rapport sexuel au moins 72 heures avant le prélèvement. <br>
+            Pas de douche, ni de toilette vaginale le jour du prélèvement <br>
+            Pas de traitement antibiotique d’aucune sorte 10 jours avant le prélèvement <br>
+            Le prélèvement doit être fait en dehors des règles.
+          </p>
+          <ul>
+            <li><b>Pour un prélèvement urétral :</b></li>
+          </ul>
+          <p style="text-align: justify;">
+            Pas de rapport sexuel au moins 72 heures avant le prélèvement. <br>
+            Pas de douche, ni de toilette intime le jour du prélèvement <br>
+            Pas de traitement antibiotique d’aucune sorte 10 jours avant le prélèvement <br>
+            Eviter d’uriner avant le prélèvement
+          </p> 
+
+          <h5 class="analyse_titre_h5">X. Autres prélèvements bactériologiques :</h5>
+          <ul>
+            <li><b>Examen cytobactériologique d’un crachat : </b></li>
+          </ul>
+          <p style="text-align: justify;">
+            Il est impératif que le recueil du crachat soit fait dans un flacon stérile fourni par le laboratoire. <br> 
+            Le recueil doit se faire avant de se brosser les dents et avant toute ingestion de liquide ou de nourriture. <br>
+            Pas de traitement antibiotique d’aucune sorte 10 jours avant le prélèvement
+          </p>
+          <ul>
+            <li><b>Examen cytobactériologique d’un prélèvement de gorge : </b></li>
+          </ul>
+          <p style="text-align: justify;">
+            Il est impératif que le prélèvement soit fait dans au laboratoire CTB. <br>
+            Pas de se brossage de dents, ni d’ingestion de liquide et de nourriture avant le prélèvement. <br>
+            Pas de traitement antibiotique d’aucune sorte 10 jours avant le prélèvement
+          </p>
+          <ul>
+            <li><b>Examen cytobactériologique d’une suppuration (pus) : </b></li>
+          </ul>
+          <p style="text-align: justify;">
+            Le prélèvement peut se faire au laboratoire à l’aide d’un écouvillon stérile <br>
+            Pas de traitement antibiotique d’aucune sorte 10 jours avant le prélèvement
+          </p>
+          <ul>
+            <li><b>Examen cytobactériologique et cytochimique d’un liquide de ponction : </b></li>
+          </ul>
+          <p style="text-align: justify;">
+            Le prélèvement ne se fait pas au laboratoire, mais en milieu hospitalier. Pour l’examen cytobactériologique, le prélèvement doit être fait dans une seringue ou un flacon stérile. Il doit également se faire en dehors de tout traitement antibiotique durant l0 jours. 
+          </p>
+          <ul>
+            <li><b>Recherche de BAAR :</b></li>
+          </ul>
+          <p style="text-align: justify;">
+            La recherche de BAAR sur crachat se fait sur 3 échantillons pendant 3 jours. <br>
+            Le recueil doit se faire avant de se brosser les dents et avant toute ingestion de liquide ou de nourriture. <br>
+            L’échantillon doit être acheminé rapidement au laboratoire. 
+          </p>         
+
+
+
+
+
+
+
+
+        
+        </div>
+
         </div>
 
       </div>
